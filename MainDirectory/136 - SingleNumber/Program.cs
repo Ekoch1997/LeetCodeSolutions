7db68c0817583120973f7f4bd81bfda8dc040d5f
Ekoch1997 <ekoch1997@gmail.com>
2022-03-01 22:55:37 -0600

    New additions

---
+++ MainDirectory/136 - SingleNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial
+{
+    public class Solution
+    {
+        public static void Main(string[] args)
+        {
+            var instance = new Solution();
+
+            int[] nums = { 2, 2, 10, 3, 4, 4, 3 };
+
+            Console.WriteLine(instance.SingleNumber(nums));
+
+        }
+
+        public int SingleNumber(int[] nums)
+        {
+            int answer = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+                answer ^= nums[i];
+
+            return answer;
+        }
+    }
+}
---
+++ MainDirectory/448 - FindAllNumbersDisappearedInAnArray/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial
+{
+    public class Solution
+    {
+        public static void Main(string[] args)
+        {
+            var instance = new Solution();
+
+            int[] nums = { 2, 2, 1, 3, 4, 4, 7 };
+
+            instance.FindDisappearedNumbers(nums);
+            instance.FindDisappearedNumbers_Solution2(nums);
+
+        }
+
+        public IList<int> FindDisappearedNumbers(int[] nums)
+        {
+            int n = nums.Length;
+            IList<int> result = new List<int>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                result.Add(i);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                result[nums[i]-1] = 0;
+            }
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                if (result[i] == 0)
+                    result.RemoveAt(i);
+            }
+
+            return result;
+        }
+
+        public IList<int> FindDisappearedNumbers_Solution2(int[] nums)
+        {
+            
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[Math.Abs(nums[i]) - 1] = -Math.Abs(nums[Math.Abs(nums[i]) - 1]);
+            }
+
+            IList<int> result = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > 0)
+                {
+                    result.Add(i + 1);
+                }
+            }
+
+            return result;
+        }
+    }
+}
